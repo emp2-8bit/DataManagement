cd2eaf45c054bfac338c391c80df77307ce6174a
emp2-8bit <59376921+emp2-8bit@users.noreply.github.com>
2023-06-09 12:08:00 +0900

    Won/lost message modified
    Won/lost message modified

--- DiceGame.c
+++ DiceGame.c
@@ -15,8 +15,8 @@ int main(){
   }
   printf("Total value: %d\n",sum);
   if(sum>7){
-    printf("name won\n");
+    printf("%s won\n",name);
   }else{
-    printf("name lost\n");
+    printf("%s lost\n",name);
   }
 }
